e134dd7d17bed26f3787c7549c67d0305b7c0286
Jesse Outh-Aut <Jesse.Outh-Aut@wolterskluwer.com>
2018-01-25 21:13:49 +1100

    change dbcontext connection to use azure sql db service

--- SlotMachineApiNetCore2/Startup.cs
+++ SlotMachineApiNetCore2/Startup.cs
@@ -33,16 +33,21 @@ namespace SlotMachineApiNetCore2
             services.AddTransient<ISlotMachineRepo, SlotMachineRepo>();
 
             // add database
-
+            // local db
             var connection =
                 @"Server=(localdb)\mssqllocaldb;Database=SlotMachineApi.Database;Trusted_Connection=True;ConnectRetryCount=0";
-            services.AddDbContext<SlotMachineContext>(options => options.UseSqlServer(connection));
+            var azureConnection = @"Data Source=slotmachineapidotnetcore2dbserver.database.windows.net;Initial Catalog=SlotMachineApiDotNetCore2_db;Integrated Security=False;User ID=jesmeister;Password=Gaib1313!;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            // azure sql db
+            // Data Source=slotmachineapidotnetcore2dbserver.database.windows.net;Initial Catalog=SlotMachineApiDotNetCore2_db;Integrated Security=False;User ID=jesmeister;Password=Gaib1313!;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            //var connection =
+            //    @"Server=tcp:slotmachineapidotnetcore2dbserver.database.windows.net,1433;Initial Catalog=SlotMachineApiDotNetCore2_db;Persist Security Info=False;User ID=jesmeister@slotmachineapidotnetcore2dbserver;Password=Gaib1313!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<SlotMachineContext>(options => options.UseSqlServer(azureConnection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             // global policy - assign here or on each controller
             app.UseCors("AllowAll");
